afe6d0a387fddc5f74c6d7cefc7c0e70d246fa12
Dom <squadrasabbia121@gmail.com>
2025-04-25 17:54:01 +0200

    aggiunto l'header main in cui ho anche creato il mazzo statico con tutte le carte

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
+#include "main.h"
 
 int main(){
-    printf("Hello World!!!\n");//palloso
-
-    int numero;
-    printf("Dammi un numero: \n");
-    scanf("%d", &numero);
-    printf("il numero e` %d",numero);
+    printf("Hello World\n");
 }
