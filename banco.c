1bde4a89a2ffaf9b5ca68450a2a34e4ab8a5d8cd
Dom <squadrasabbia121@gmail.com>
2025-04-25 19:28:59 +0200

    modificata funzione pesca e rimosso puntatori

--- banco.c
+++ banco.c
@@ -4,18 +4,16 @@
 
 int manoBanco[] = {0};
 
-void pesca(int *mano[]){
+void pesca(int mano[]){
       srand(time(NULL));
 
-      for(int i = 0; i < sizeof(*mano) / sizeof(*mano[0]); i++){
+      for(int i = 0; i < sizeof(mano) / sizeof(mano[0]) + 1; i++){
           short temp = rand() + 1 % 52;
 
-             if(*mano[i] == mazzo[temp]){
+             if(mano[i] == mazzo[temp]){
                continue;
              }
-             if(*mano[i] == 0){
-                 *mano[i] = mazzo[temp];
-             }
+
       }
 
 
