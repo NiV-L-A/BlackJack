67a1329804997ec841d919d0f49c1d6c33ce39f3
Dom <squadrasabbia121@gmail.com>
2025-04-25 20:36:41 +0200

    errore

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include "banco.c"
 #include "main.h"
 
 int main(){//nel main mettiamo solo le call alle funzioni.
-    for (int i = 0; i < 52; i++) {
-        printf("%03d\n", mazzo[i]);
+   static int manoBanco[22];
+    int manoGiocatore[22];
+
+    pesca(manoBanco);
+    pesca(manoBanco);
+    pesca(manoBanco);
+    pesca(manoBanco);
+
+
+
+    for (int i = 0; i < 22; i++) {
+        if (manoBanco[i] != 0) {
+            printf("%d\n", manoBanco[i]);
+        }
     }
 }
